5be820d7edaf2c4e2386c06c8947b464c2d0c632
officialcbk <eeze@academic.rrc.ca>
2025-11-04 23:04:33 -0600

    on add contact method completed

--- contact_list/contact_list.py
+++ contact_list/contact_list.py
@@ -4,7 +4,9 @@ __author__ = "ACE Faculty"
 __version__ = "1.0.0"
 __credits__ = ""
 
-from PySide6.QtWidgets import  QMainWindow, QLineEdit, QPushButton, QTableWidget, QLabel, QVBoxLayout, QWidget, QTableWidgetItem
+from PySide6.QtWidgets import  QMainWindow, QLineEdit, QPushButton
+from PySide6.QtWidgets import QTableWidget, QLabel, QVBoxLayout, QWidget, QTableWidgetItem
+from PySide6.QtCore import Slot
 
 class ContactList(QMainWindow):
     """Represents a window that provides the UI to manage contacts."""
@@ -48,3 +50,24 @@ class ContactList(QMainWindow):
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
+        
+        self.add_button.clicked.connect(self.__on_add_contact)
+        
+    @Slot()
+    def __on_add_contact(self):
+        """ Private method to handle adding a contact """
+
+        name = self.contact_name_input.text().strip()
+        phone = self.phone_input.text().strip()
+
+        if name and phone:
+            row_position = self.contact_table.rowCount()
+
+            self.contact_table.insertRow(row_position)
+
+            self.contact_table.setItem(row_position, 0, QTableWidgetItem(name))
+            self.contact_table.setItem(row_position, 1, QTableWidgetItem(phone))
+
+            self.status_label.setText(f"Added contact: {name}")
+        else:
+            self.status_label.setText("Please enter a contact name and phone number.")
